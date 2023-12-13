349194fdf5b6e4f947c0253c0318f3214a51f533
negisuraj080 <143009358+negisuraj080@users.noreply.github.com>
2023-12-13 12:58:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 const makeChange = (c) => {
   // your name here
+	const quarterValue = 25;
+  const dimeValue = 10;
+  const nickelValue = 5;
+  const pennyValue = 1;
+
+  // Initialize coin counts
+  let quarters = Math.floor(amount / quarterValue);
+  let remainder = amount % quarterValue;
+
+  let dimes = Math.floor(remainder / dimeValue);
+  remainder %= dimeValue;
+
+  let nickels = Math.floor(remainder / nickelValue);
+  remainder %= nickelValue;
+
+  let pennies = remainder;
+
+  // Create and return the result object
+  const result = {
+    "q": quarters,
+    "d": dimes,
+    "n": nickels,
+    "p": pennies
+  };
+
+  return result;
 };
 
 // Do not the change the code below
